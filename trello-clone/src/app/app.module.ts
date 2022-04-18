3cb887543e2f4f394e10e2843d7cebbb73c165c4
xiang1li <venusli370306@gmail.com>
2022-04-18 19:21:23 +0800

    Adding homepage and nav bar to Trello Minimal

--- trello-clone/src/app/app.module.ts
+++ trello-clone/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SharedModule } from './shared/shared.module';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomePageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- trello-clone/src/app/shared/shell/shell.component.ts
+++ trello-clone/src/app/shared/shell/shell.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
-  styleUrls: ['./shell.component.scss']
+  styleUrls: ['./shell.component.scss'],
 })
-export class ShellComponent implements OnInit {
+export class ShellComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe([Breakpoints.Handset])
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
